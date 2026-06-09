dec843872d9a35c9de40cef96d480fdf243e57fa
juan <jibyrab@gmail.com>
2026-06-09 16:36:58 -0300

    fix mailto message

--- js/contact.js
+++ js/contact.js
@@ -2,23 +2,20 @@ document.getElementById('mailto-form').addEventListener('submit', function(e) {
     e.preventDefault();  // Evita que recargue la página
 
     const nombre = document.getElementById('nombre').value.trim();
-    const email = document.getElementById('email').value.trim();
     const mensaje = document.getElementById('mensaje').value.trim();
 
-    if (!nombre || !email || !mensaje) {
+    if (!nombre) {
         alert('Por favor completa todos los campos.');
         return;
     }
-
-    // Construir el cuerpo del correo (puedes personalizarlo)
-    const cuerpo = `Nombre: ${nombre}%0A` +
-                   `Correo del remitente: ${email}%0A%0A` +
-                   `Mensaje:%0A${mensaje}`;
-
+    if (!mensaje) {
+        mensaje =  `Hola, mi nombre es ${nombre}.\n\nQuisiera contactarte para hablar sobre una posible proyecto. Cuando tengas un momento, por favor revisa mi mensaje. ¡Gracias!`;
+    }
+    
     // Tu dirección de correo (cámbiala por la tuya)
     const destinatario = 'juan.brecevich.48@gmail.com';
     const asunto = `Contacto desde mi portfolio - ${nombre}`;
 
     // Crear el enlace mailto:
-    window.location.href = `mailto:${destinatario}?subject=${encodeURIComponent(asunto)}&body=${cuerpo}`;
+    window.location.href = `mailto:${destinatario}?subject=${encodeURIComponent(asunto)}&body=${encodeURIComponent(mensaje)}`;
 });
