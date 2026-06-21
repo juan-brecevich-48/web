c00cbfe162b422ee8a1f196a6ae13841203cccfb
juan <jibyrab@gmail.com>
2026-06-21 20:53:30 -0300

    toast + js

--- contact.js
+++ contact.js
@@ -1,20 +1,14 @@
 document.getElementById('mailto-form').addEventListener('submit', function(e) {
     e.preventDefault();  // Evita que recargue la página
 
-    const nombre = document.getElementById('nombre').value.trim();
     const mensaje = document.getElementById('mensaje').value.trim();
-
-    if (!nombre) {
-        alert('Por favor completa todos los campos.');
-        return;
-    }
     if (!mensaje) {
         mensaje =  `Hola, mi nombre es ${nombre}.\n\nQuisiera contactarte para hablar sobre una posible proyecto. Cuando tengas un momento, por favor revisa mi mensaje. ¡Gracias!`;
     }
     
     // Tu dirección de correo (cámbiala por la tuya)
     const destinatario = 'juan.brecevich.48@gmail.com';
-    const asunto = `Contacto desde mi portfolio - ${nombre}`;
+    const asunto = `Contacto desde mi portfolio`;
 
     // Crear el enlace mailto:
     window.location.href = `mailto:${destinatario}?subject=${encodeURIComponent(asunto)}&body=${encodeURIComponent(mensaje)}`;
