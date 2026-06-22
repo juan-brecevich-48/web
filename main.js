960ca02b1abdd0798d077e8fdb38ff1d20f41eda
juan <jibyrab@gmail.com>
2026-06-21 22:35:16 -0300

    emailjs bugs

--- main.js
+++ main.js
@@ -2,42 +2,49 @@ emailjs.init({
     publicKey: '65VL7490KAhpkzypw'
 });
 
-document.getElementById('contactForm').addEventListener('submit', function (e) {
-    e.preventDefault();  // Evita que recargue la página
-
-    const nombre = document.getElementById('nombre').value.trim();
-    const correo = document.getElementById('correo').value.trim();
-    let message = document.getElementById('mensaje').value.trim();
-    if (!nombre || !correo) {
-        showToast('Por favor, completa tu nombre y correo electrónico.', 'danger');
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('contactForm');
+    if (!form) {
+        console.error('Form element not found!');
         return;
     }
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
 
-    if (!message) {
-        message = `Hola, soy ${nombre}.\n\nQuisiera contactarte para hablar sobre una posible proyecto. Cuando tengas un momento, por favor revisa mi mensaje. ¡Gracias!`;
-    }
+        const nombre = document.getElementById('nombre').value.trim();
+        const correo = document.getElementById('correo').value.trim();
+        let messageForm = document.getElementById('mensaje').value.trim();
+        if (!nombre || !correo) {
+            showToast('Por favor, completa tu nombre y correo electrónico.', 'danger');
+            return;
+        }
 
-    // Dirección de correo
-    const destinatario = 'juan.brecevich.48@gmail.com';
-    const asunto = `Contacto desde mi portfolio`;
+        if (!messageForm) {
+            messageForm = `Hola, soy ${nombre}.\n\nQuisiera contactarte para hablar sobre una posible proyecto. Cuando tengas un momento, por favor revisa mi mensaje. ¡Gracias!`;
+        }
 
-    const templateParams = {
-        name: nombre,
-        email: correo,
-        message: message
-    };
+        // Dirección de correo
+        const destinatario = 'juan.brecevich.48@gmail.com';
+        const asunto = `Contacto desde mi portfolio`;
 
-    // Enviar el formulario
-    emailjs.sendForm('contact_service', 'contact_template', templateParams)
-        .then(() => {
-            showToast('Correo enviado correctamente');
-            // Resetear el formulario
-            this.reset();
-        }, (err) => {
-            // Error
-            console.error('Error al enviar:', err);
-            showToast('Hubo un error al enviar el mensaje. Intenta de nuevo.', 'danger');
-        });
+        const templateParams = {
+            name: nombre,
+            email: correo,
+            message: messageForm
+        };
+
+        // Enviar el formulario
+        emailjs.send('contact_service', 'contact_template', templateParams)
+            .then(() => {
+                showToast('Correo enviado correctamente');
+                // Resetear el formulario
+                this.reset();
+            }, (err) => {
+                // Error
+                console.error('Error al enviar:', err);
+                showToast('Hubo un error al enviar el mensaje. Intenta de nuevo.', 'danger');
+            });
+    })
 });
 
 async function copyToClipboard(text, type) {
